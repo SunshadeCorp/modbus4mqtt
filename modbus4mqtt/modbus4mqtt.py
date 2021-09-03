a9da8be984e21bf6a154bfd3322da6c18d028aa1
Jesse Kleemann <jesse.kleemann@duesenfeld.com>
2021-09-03 23:46:58 +0200

    test mask

--- modbus4mqtt/modbus4mqtt.py
+++ modbus4mqtt/modbus4mqtt.py
@@ -108,7 +108,7 @@ class MqttInterface:
             # Filter the value through the mask, if present.
             value &= register.get('mask', 0xFFFF)
             if register.get('mask', 0xFFFF) != 0xFFFF:
-                logging.info(f"{type(someObject).__name__} {register.get('mask', 0xFFFF)}")
+                logging.info(f"{type(register.get('mask', 0xFFFF)).__name__} {register.get('mask', 0xFFFF)}")
             # Tweak the value according to the type.
             datatype = register.get('type', 'uint16')
             if datatype.endswith('32'):
