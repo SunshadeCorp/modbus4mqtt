e6f0c44c8f0f621580d7eb1a5d65c1c7c7a823b8
Jesse Kleemann <jesse.kleemann@duesenfeld.com>
2021-09-06 11:20:18 +0200

    check scanned ranges

--- modbus4mqtt/modbus_interface.py
+++ modbus4mqtt/modbus_interface.py
@@ -61,6 +61,7 @@ class ModbusInterface:
         self._tables[table].add(addr)
 
     def poll(self):
+        logging.info("start poll")
         # Polls for the values marked as interesting in self._tables.
         for table in self._tables:
             # This batches up modbus reads in chunks of self._scan_batching
@@ -69,7 +70,7 @@ class ModbusInterface:
                 group = int(k) - (int(k) + 1) % self._scan_batching
                 if start < group:
                     try:
-                        logging.exception(f"group: {group}")
+                        logging.info(f"group: {group}")
                         values = self._scan_value_range(table, group, self._scan_batching)
                         for x in range(0, self._scan_batching):
                             key = group + x
@@ -80,6 +81,7 @@ class ModbusInterface:
                         logging.exception(f"{e}")
                     start = group + self._scan_batching - 1
         self._process_writes()
+        logging.info("end poll")
 
     def get_value(self, table, addr):
         if table not in self._values:
